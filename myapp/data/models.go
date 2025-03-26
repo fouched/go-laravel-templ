62b96ff169afc9b769dcd1c3588ab591dbd9c771
Fouché du Preez <fouche@unifi.credit>
2025-03-26 10:31:56 +0200

    use local db var

--- myapp/data/models.go
+++ myapp/data/models.go
@@ -23,9 +23,9 @@ func New(databasePool *sql.DB) Models {
 	db = databasePool
 
 	if os.Getenv("DATABASE_TYPE") == "mysql" || os.Getenv("DATABASE_TYPE") == "mariadb" {
-		upper, _ = mysql.New(databasePool)
+		upper, _ = mysql.New(db)
 	} else {
-		upper, _ = postgresql.New(databasePool)
+		upper, _ = postgresql.New(db)
 	}
 
 	return Models{
