a74355227e4809f7b674433a9ba84156d872135d
Fouche du Preez <fouche@limehouse.co.za>
2025-04-18 17:10:20 +0200

    added auth support

--- rapidus/cmd/cli/auth.go
+++ rapidus/cmd/cli/auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"time"
 )
 
@@ -38,6 +39,23 @@ func doAuth() error {
 	if err != nil {
 		exitGracefully(err)
 	}
+
+	// copy middleware
+	err = copyFileFromTemplate("templates/middleware/auth.go.txt", rap.RootPath+"/middleware/auth.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/middleware/auth-token.go.txt", rap.RootPath+"/middleware/auth-token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	color.Yellow("  - users, tokens and remember_tokens migrations created and executed")
+	color.Yellow("  - user and token models created")
+	color.Yellow("  - auth middleware created")
+	color.Yellow("")
+	color.Yellow("  - Don't forget to add user and token models in data/models.go, and to add appropriate middleware to your routes!")
 
 	return nil
 }
