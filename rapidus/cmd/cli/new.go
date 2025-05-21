5d988f17ea567ee14656c2204e43f831a4e64e7e
Fouche du Preez <fouche@limehouse.co.za>
2025-05-21 20:56:01 +0200

    cli enhancements - wip

--- rapidus/cmd/cli/new.go
+++ rapidus/cmd/cli/new.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
 	"log"
@@ -19,7 +20,7 @@ func doNew(appName string) {
 	log.Println("App name is", appName)
 
 	// git clone skeleton application
-	color.Green("\tCloning repository")
+	color.Yellow("\tCloning repository...")
 	_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
 		URL:      "https://github.com/fouched/rapidus-app.git",
 		Progress: os.Stdout,
@@ -30,8 +31,26 @@ func doNew(appName string) {
 	}
 
 	// remove .git directory
+	err = os.RemoveAll(fmt.Sprintf("./%s/.git", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// create a ready to go .env file
+	color.Yellow("\tCreating .env file...")
+	data, err := templateFS.ReadFile("templates/env.txt")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	env := string(data)
+	env = strings.ReplaceAll(env, "${APP_NAME}", appName)
+	env = strings.ReplaceAll(env, "${KEY}", rap.RandomString(32))
+
+	err = copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// create a makefile
 
