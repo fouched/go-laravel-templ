df29fb6531661b9670484c92e528e758bfc2ce20
Fouche du Preez <fouche@limehouse.co.za>
2025-05-23 21:31:54 +0200

    update source files

--- rapidus/cmd/cli/new.go
+++ rapidus/cmd/cli/new.go
@@ -97,6 +97,7 @@ func doNew(appName string) {
 
 	// update existing .go files with correct name/imports
 	color.Yellow("\tUpdating source files...")
+	os.Chdir("./" + appName)
 	updateSource()
 
 	// run go mod tidy in project directory
